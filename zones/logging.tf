d0aaeef610dd25441e0f2f8c614529f2f476d519
Boris Yakimov <boris.yakimov@itgix.com>
2026-03-13 17:29:05 +0200

    #243 remove reference to local var

--- zones/logging.tf
+++ zones/logging.tf
@@ -1,11 +1,8 @@
-locals {
-  zone_name = replace(lookup(each.value, "domain_name", each.key), ".", "-")
-}
-
 resource "aws_route53_resolver_query_log_config" "itgix_landing_zone" {
   for_each = { for k, v in var.zones : k => v if var.create }
+  // replace the . char in the the domain with -, exmaple lz.itgix.eu becomes lz-itgix-eu
+  name = "${replace(lookup(each.value, "domain_name", each.key), ".", "-")}-dns-query-logs"
 
-  name = "${local.zone_name}-dns-query-logs"
   // destination for query logs can be S3, Cloudwatch, or Kinesis Data Firehose
   destination_arn = aws_cloudwatch_log_group.itgix_landing_zone[each.key].arn
 
@@ -18,7 +15,9 @@ resource "aws_route53_resolver_query_log_config" "itgix_landing_zone" {
 resource "aws_cloudwatch_log_group" "itgix_landing_zone" {
   for_each = { for k, v in var.zones : k => v if var.create }
 
-  name              = "${local.zone_name}-dns-query-logs"
+  // replace the . char in the the domain with -, exmaple lz.itgix.eu becomes lz-itgix-eu
+  name = "${replace(lookup(each.value, "domain_name", each.key), ".", "-")}-dns-query-logs"
+
   retention_in_days = min(var.log_retention_days, 365)
 
   tags = merge(
