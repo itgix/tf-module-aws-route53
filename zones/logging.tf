d69bfac7b000ce521294876fd11054aebd7dcd75
Boris Yakimov <boris.yakimov@itgix.com>
2026-03-13 16:59:14 +0200

    #243 add dns query logs

---
+++ zones/logging.tf
@@ -0,0 +1,23 @@
+resource "aws_route53_resolver_query_log_config" "itgix_landing_zone" {
+  for_each = { for k, v in var.zones : k => v if var.create }
+
+  name = "${lookup(each.value, "domain_name", each.key)}-dns-query-logs"
+  // destination for query logs can be S3, Cloudwatch, or Kinesis Data Firehose
+  destination_arn = aws_cloudwatch_log_group.itgix_landing_zone[each.key].arn
+
+  tags = merge(
+    lookup(each.value, "tags", {}),
+    var.tags
+  )
+}
+
+resource "aws_cloudwatch_log_group" "itgix_landing_zone" {
+  for_each = { for k, v in var.zones : k => v if var.create }
+
+  name = "${lookup(each.value, "domain_name", each.key)}-dns-query-logs"
+
+  tags = merge(
+    lookup(each.value, "tags", {}),
+    var.tags
+  )
+}
